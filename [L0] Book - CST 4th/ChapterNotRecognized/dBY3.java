a6a51100e50e7cb5fd1ca4e0cd0fc1d7968ac4a5
Rayhan Ahmed <imrayniloy@gmail.com>
2024-11-17 18:38:47 +0600

    commin dBY3

--- [L0] Book - CST 4th/ChapterNotRecognized/dBY3.java
+++ [L0] Book - CST 4th/ChapterNotRecognized/dBY3.java
@@ -1,3 +1,46 @@
+import java.util.Scanner;
+
 public class dBY3 {
-    
+    public static void main (String[] args) {
+        //config utilities
+        Scanner sc = new Scanner(System.in);
+        
+        //dynamic approch, by taking an input
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+
+        // call the validator function
+        int digit = validator(num);
+
+        // check if the number is divisible by 3 or not
+        if(digit % 3 == 0) 
+        {
+            System.out.println("The number " + num + " is divisible by 3");
+        } else {
+            System.out.println("The number " + num + " is not divisible by 3");
+        }
+
+        sc.close();
+    }
+
+    // Function to validate the number
+    public static int validator(int num)
+    {
+        while (num > 9) {
+            num = combineSum(num);
+        }
+        return num;
+    }
+
+    // Function to combine the sum of the digits
+    public static int combineSum(int num)
+    {
+        int sum = 0; 
+        while (num != 0) {
+            sum += num % 10; //Extract last digit from num & add the last digit to the sum
+            num /= 10; //Remove the last digit, which is a fraction
+        }
+        return sum;
+
+    }
 }
